52d5d60fdbb0afc5aa64996b3124008dc5b97e3c
branislav999 <bbogosavac1999@gmail.com>
2024-04-02 02:21:29 -0600

    membersjson

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -7,3 +7,41 @@ menuButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     menuButton.classList.toggle('open');
 })
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var sidebar = document.getElementById("sidebar");
+    var messageDiv = document.getElementById("message");
+
+    var lastVisit = localStorage.getItem("lastVisit");
+
+    var currentTime = Date.now();
+
+    if (lastVisit) {
+        var timeDifference = currentTime - parseInt(lastVisit);
+
+        var daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+        if (daysDifference === 0) {
+            messageDiv.textContent = "Back so soon! Awesome!";
+        } else {
+            var message;
+            if (daysDifference === 1) {
+                message = "You last visited 1 day ago.";
+            } else {
+                message = "You last visited " + daysDifference + " days ago.";
+            }
+            messageDiv.textContent = message;
+        }
+    } else {
+
+        messageDiv.textContent = "Welcome! Let us know if you have any questions.";
+    }
+
+    localStorage.setItem("lastVisit", currentTime);
+});
+
+
+
+
+
